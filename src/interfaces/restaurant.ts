3264cb9b7b9631fde06e76e4d1226904d094aa10
Diego Saggioro <diegosaggiorobrito@gmail.com>
2025-05-11 00:05:11 -0300

    feat: adicionado tipagens para user e restaurant

---
+++ src/interfaces/restaurant.ts
@@ -0,0 +1,42 @@
+interface IOption {
+  name: string;
+  price: number;
+  discountedPrice?: number;
+}
+
+interface IExtra {
+  name: string;
+  limitChoice: number;
+  minimumChoice: number;
+  options: IOption[];
+}
+
+interface IMenuItem {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  discountedPrice?: number;
+  extras?: IExtra[];
+  flags?: string[];
+}
+
+interface IMenu {
+  id: number;
+  name: string;
+  description?: string;
+  items: IMenuItem[];
+}
+
+interface IRestaurant {
+  id: number;
+  name: string;
+  address: string;
+  phone: string;
+  cuisine: string;
+  open: boolean;
+  logo: string;
+  review: number;
+  shippingPrice: number;
+  menus: IMenu[];
+}
