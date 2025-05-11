84b7b7e16e89f1424c1cf6240db2de4b68222a51
Diego Saggioro <diegosaggiorobrito@gmail.com>
2025-05-11 00:08:48 -0300

    feat: adicionado componente de lista otimizada com react-window

---
+++ src/components/FlatList.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { getViewHeight } from "@/utils/viewUtils";
+import React, { ComponentType } from "react";
+import { FixedSizeList, ListChildComponentProps } from "react-window";
+
+interface IFlatListProps {
+  height: number;
+  width: number;
+  itemCount: number;
+  itemSize: number;
+  Row: ComponentType<ListChildComponentProps<any>>;
+  itemData: {
+    items: any[];
+    onClick?: (id: number) => void;
+  };
+}
+
+const FlatList: React.FC<IFlatListProps> = ({
+  height,
+  width,
+  itemCount,
+  itemSize,
+  Row,
+  itemData,
+}) => {
+  return (
+    <FixedSizeList
+      height={height}
+      width={width}
+      itemCount={itemCount}
+      itemSize={itemSize}
+      itemData={itemData}
+    >
+      {Row}
+    </FixedSizeList>
+  );
+};
+
+export default FlatList;
