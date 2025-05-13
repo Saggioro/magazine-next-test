283f65f64b4021a18a77dfc9f0393c9d2ea98460
Diego Saggioro <diegosaggiorobrito@gmail.com>
2025-05-12 23:50:50 -0300

    refactor: refatorado opcao de item para reduzir tamanho da pagina de item e melhor leitura

--- src/app/restaurant/[id]/item/[iditem]/[foodid]/page.tsx
+++ src/app/restaurant/[id]/item/[iditem]/[foodid]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Button from "@/components/Button";
+import ItemOption from "@/components/ItemOption";
 import SelectInput from "@/components/SelectInput";
 import { ThemedText } from "@/components/ThemedText";
 import { useViewSize } from "@/hooks/useViewSize";
@@ -282,65 +283,13 @@ const RestaurantItem: React.FC = () => {
                 </div>
                 {extra.options.map((option) => {
                   return (
-                    <div
-                      className="mt-6 mb-2 flex flex-row items-center justify-between"
+                    <ItemOption
+                      extra={extra}
+                      option={option}
+                      handleClick={() => handleSelect(extra, option)}
+                      selected={checkSelected(extra, option)}
                       key={option.name}
-                    >
-                      <div
-                        className="flex flex-row items-center"
-                        onClick={() => handleSelect(extra, option)}
-                      >
-                        <SelectInput
-                          selected={checkSelected(extra, option)}
-                          type={extra.limitChoice > 1 ? "square" : "circle"}
-                        />
-                        {option.discountedPrice && (
-                          <Image
-                            alt="Desconto"
-                            src={"/discount.svg"}
-                            width={16}
-                            height={16}
-                            className="ml-4"
-                            loading="lazy"
-                          />
-                        )}
-                        <ThemedText
-                          color="subtitle"
-                          extraClasses={
-                            option.discountedPrice ? "ml-2" : "ml-4"
-                          }
-                        >
-                          {option.name}
-                        </ThemedText>
-                      </div>
-                      {option.price > 0 && (
-                        <div>
-                          {option.discountedPrice ? (
-                            <div className="flex flex-row items-end">
-                              <ThemedText
-                                type="bold"
-                                color="subtitle"
-                                size="small"
-                                extraClasses="mr-1"
-                              >
-                                de R$ {option.price.toFixed(2)} por
-                              </ThemedText>
-                              <ThemedText type="bold" color="discount">
-                                R$ {option.discountedPrice.toFixed(2)}
-                              </ThemedText>
-                            </div>
-                          ) : (
-                            <ThemedText
-                              type="bold"
-                              color="secondaryTitle"
-                              extraClasses="ml-4"
-                            >
-                              + R$ {option.price.toFixed(2)}
-                            </ThemedText>
-                          )}
-                        </div>
-                      )}
-                    </div>
+                    />
                   );
                 })}
               </div>
---
+++ src/components/ItemOption.tsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import SelectInput from "./SelectInput";
+import { ThemedText } from "./ThemedText";
+
+interface IItemOptionProps {
+  option: IOption;
+  extra: IExtra;
+  selected: boolean;
+  handleClick: (extra: IExtra, option: IOption) => void;
+}
+
+const ItemOption: React.FC<IItemOptionProps> = ({
+  option,
+  extra,
+  handleClick,
+  selected,
+}) => {
+  return (
+    <div
+      className="mt-6 mb-2 flex flex-row items-center justify-between"
+      key={option.name}
+    >
+      <div
+        className="flex flex-row items-center"
+        onClick={() => handleClick(extra, option)}
+      >
+        <SelectInput
+          selected={selected}
+          type={extra.limitChoice > 1 ? "square" : "circle"}
+        />
+        {option.discountedPrice && (
+          <Image
+            alt="Desconto"
+            src={"/discount.svg"}
+            width={16}
+            height={16}
+            className="ml-4"
+            loading="lazy"
+          />
+        )}
+        <ThemedText
+          color="subtitle"
+          extraClasses={option.discountedPrice ? "ml-2" : "ml-4"}
+        >
+          {option.name}
+        </ThemedText>
+      </div>
+      {option.price > 0 && (
+        <div>
+          {option.discountedPrice ? (
+            <div className="flex flex-row items-end">
+              <ThemedText
+                type="bold"
+                color="subtitle"
+                size="small"
+                extraClasses="mr-1"
+              >
+                de R$ {option.price.toFixed(2)} por
+              </ThemedText>
+              <ThemedText type="bold" color="discount">
+                R$ {option.discountedPrice.toFixed(2)}
+              </ThemedText>
+            </div>
+          ) : (
+            <ThemedText type="bold" color="secondaryTitle" extraClasses="ml-4">
+              + R$ {option.price.toFixed(2)}
+            </ThemedText>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ItemOption;
